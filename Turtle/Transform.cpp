c957ba9f3ea55e019d4776a49e65c33d38b16d45
UntitledProgrammer <thomasjacobs0257@gmail.com>
2022-09-04 22:19:13 +0100

    Transform optimisation.

--- Turtle/Transform.cpp
+++ Turtle/Transform.cpp
@@ -4,14 +4,5 @@ Transform::Transform() : translation(glm::vec3(0,0,0)), rotation(glm::vec3(0,0,0
 
 glm::mat4 Transform::GetMatrix()
 {
-	glm::mat4 transMatrix = glm::translate(translation);
-
-	glm::mat4 rotXMatrix = glm::rotate(rotation.x, glm::vec3(1, 0, 0));
-	glm::mat4 rotYMatrix = glm::rotate(rotation.y, glm::vec3(0, 1, 0));
-	glm::mat4 rotZMatrix = glm::rotate(rotation.z, glm::vec3(0, 0, 1));
-	glm::mat4 scaleMatrix = glm::scale(scale);
-
-	glm::mat4 rotMatrix = rotZMatrix * rotYMatrix * rotXMatrix;
-
-	return transMatrix * rotMatrix * scaleMatrix;
+	return glm::translate(translation) * (glm::rotate(rotation.z, depth) * glm::rotate(rotation.y, vertical) * glm::rotate(rotation.x, horizontal)) * glm::scale(scale);
 }
--- Turtle/Transform.h
+++ Turtle/Transform.h
@@ -9,6 +9,11 @@
 class Transform
 {
 public:
+	//Static memebers:
+	static constexpr glm::vec3 horizontal = glm::vec3(1, 0, 0);
+	static constexpr glm::vec3 vertical = glm::vec3(0, 1, 0);
+	static constexpr glm::vec3 depth = glm::vec3(0, 0, 1);
+
 	//Attributes:
 	glm::vec3 translation;
 	glm::vec3 rotation;
